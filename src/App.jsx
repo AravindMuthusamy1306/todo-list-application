d0d45d16ed5a756d0c366aaa93697acbcf65d53e
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-05-09 17:59:13 +0530

    Todo App Completed

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css'
+import {useState} from 'react'
+function App() {
+  const[todoList, setTodoList] = useState([])
+  const[newTask, setNewTask] = useState("")
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  }
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false
+    }
+    setTodoList([...todoList, task])
+    setNewTask("")
+  }
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id))
+  }
+  const completeTask = (id) =>{
+    setTodoList(todoList.map((task) => task.id === id ? {...task, completed : !task.completed } : task))
+  }
+  return(
+      <div className="App">
+        <div className="addTask">
+          <input value={newTask} onChange={handleChange}/>
+          <button onClick={addTask}> Add Task </button>
+        </div>
+        <div className="taskList">
+          {
+            todoList.map((task, index) =>{
+              return(
+                <div key={task.id} style={{backgroundColor : task.completed ? "green" : "none" }}>
+                  {task.taskName}
+                  <button onClick= {() => completeTask(task.id)}> Completed </button>
+                  <button onClick={()=>deleteTask(task.id)}> X </button>
+                </div>
+              )
+            } )
+          }
+        </div>
+      </div>
+  );
+}
+
+export default App
