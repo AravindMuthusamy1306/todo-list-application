fd90579430bb40c29bab3fe3ab921f84431a087a
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-05-11 17:13:06 +0530

    Task Component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import { Task } from './Task'; // ✅ Import the new component
+import { Task } from './Task';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
