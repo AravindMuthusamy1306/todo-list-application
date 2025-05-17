5535f4dba53ad59901ede597f60a468338d038f1
AravindMuthusamy1306 <aravindmuthusamy188@gmail.com>
2025-05-17 18:58:07 +0530

    changes

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,6 +1,6 @@
 export function Task({ task, completeTask, deleteTask }) {
   return (
-    <div style={{ backgroundColor: task.completed ? "green" : "none" }}>
+    <div style={{ backgroundColor: task.completed ? "green" : "white" }}>
       <h1>{task.taskName}</h1>
       <button onClick={() => completeTask(task.id)}>Completed</button>
       <button onClick={() => deleteTask(task.id)}>X</button>
